e5ee6db9265b68c421e03291e477d3b5556286bc
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2023-04-20 10:33:49 +0800

    [MOD] transfer notes to notepad

--- helloWorld/routes/web.php
+++ helloWorld/routes/web.php
@@ -17,21 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello',function(){
-    return response('<h1>Hello World</h1>', 200)
-        -> header('Content-Type', 'text/plain')
-        -> header('foo', 'bar');
-});
-
-// wildcard with contraint that will accept only numbers
-// to accept only number, will use regular expression
-// ddd -> die dump and debug while dd -> die and dump
-Route::get('/posts/{id}', function($id){
-    return response('Post '.$id);
-})-> where('id', '[0-9]+');
-
-// Request and Query Params
-Route::get('/search', function(Request $request){
-    return ($request->name.' '.$request->city);
-});
